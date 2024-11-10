f304dbc31f08c2cdb5a87b90229c249b5148b85d
Aysanat <aysanat21aysanat@gmail.com>
2024-11-10 20:57:05 +0300

    Доделала header_burger, добавила скрипты

--- www/scripts/script.js
+++ www/scripts/script.js
@@ -1,5 +1,28 @@
 $(document).ready(function() {
+  let $menu = $('.js-menu');
+
   $('.js-burger').on('click', function() {
-    $('.js-menu').show();
+    $menu.slideToggle();
   });
+
+  let prevAccordionBtn;
+  let openClass = 'open';
+
+  $('.js-accordion-btn').on('click', function() {
+    if (this === prevAccordionBtn) {
+      $(this).toggleClass(openClass)
+      $(this).next().slideToggle();
+      return;
+    }
+
+    $(prevAccordionBtn).removeClass(openClass);
+    $(prevAccordionBtn).next().slideUp;
+
+    $(this).addClass(openClass)
+    $(this).next().slideDown();
+
+    prevAccordionBtn = this;
+  });
+
+
 });
